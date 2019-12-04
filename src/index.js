ba57d2ddf941798d295e0e535d9ce92e0d668895
Jakub Dostál <dostal.jakub@outlook.com>
2019-12-04 09:59:16 +0100

    Lifting state up

--- src/index.js
+++ src/index.js
@@ -2,26 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/**
+ * Square
+ */
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  // private
-            value: null
-        };
-    }
-
     render() {
         return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                {this.state.value}
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
             </button>
         );
     }
 }
 
+//Note
+// The DOM <button> element’s onClick attribute has a special meaning to React because it is a built-in component.
+// For custom components like Square, the naming is up to you. We could give any name to the Square’s onClick prop
+// or Board’s handleClick method, and the code would work the same. In React, it’s conventional to use on[Event]
+// names for props which represent events and handle[Event] for the methods which handle the events.
+
+/**
+ * Board
+ */
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+    // When we fill the board in later, the this.state.squares array will look something like this:
+    //     [
+    //         'O', null, 'X',
+    //         'X', 'X', 'O',
+    //         'O', null, null,
+    //     ]
+
+    handleClick(i){
+        // we call .slice() to create a copy of the squares array to modify instead of modifying the existing array
+        const squares =  this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i}/>;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
@@ -50,6 +80,10 @@ class Board extends React.Component {
     }
 }
 
+
+/**
+ * Game
+ */
 class Game extends React.Component {
     render() {
         return (
