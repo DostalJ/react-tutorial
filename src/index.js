011ece0c0132b2d3bce7cd4cea72a6bd8ac3a4d3
Jakub Dostál <dostal.jakub@outlook.com>
2019-12-04 10:14:24 +0100

    Declaring a Winner

--- src/index.js
+++ src/index.js
@@ -42,6 +42,10 @@ class Board extends React.Component {
     handleClick(i) {
         // we call .slice() to create a copy of the squares array to modify instead of modifying the existing array
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]){
+            // Pokud uz nekdo vyhral, nebo uz je pole obsazene.
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({squares: squares, xIsNext: !this.state.xIsNext});
     }
@@ -56,7 +60,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -107,3 +117,24 @@ ReactDOM.render(
     <Game/>,
     document.getElementById('root')
 );
+
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && (squares[b] === squares[b]) && (squares[a] === squares[c])) {
+            return squares[a]
+        }
+    }
+    return null
+}
